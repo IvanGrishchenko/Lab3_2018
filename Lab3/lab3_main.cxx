157cf79f71be10c2778aa0ba66291eb03de8167f
Oleksandr Maistrenko <al.master2000@gmail.com>
2018-11-25 15:02:35 +0200

    Виправлено реалізацію багаторазового запуску програми

--- Lab3/lab3_main.cxx
+++ Lab3/lab3_main.cxx
@@ -14,44 +14,54 @@ void drawDot(sf::RenderWindow& window, sf::Vertex dot){
     window.draw(shape);
 }
 
+void drawInfo(sf::RenderWindow& window, const unsigned int x){
+    sf::Font font;
+    font.loadFromFile("times.ttf");
+    sf::Text text("seed = " + to_string(x), font);
+    text.setCharacterSize(20);
+    text.setFillColor(sf::Color::Black);
+    text.setPosition(10,10);
+    window.draw(text);
+}
+
+sf::VertexArray generateDots(){
+    sf::VertexArray dot(sf::Points, amount);
+    for(int i = 0; i < amount; ++i){
+        float a = static_cast<float>(100+rand()%800);
+        float b = static_cast<float>(100+rand()%400);
+        dot[i].position = sf::Vector2f(a, b);
+    }
+    return dot;
+}
 int main(){
-    for(unsigned int s = 0; s < tests; ++s){
-        sf::RenderWindow window(sf::VideoMode(1000, 600), "Lab3");
-
-        unsigned int x = time(0)+s;
-        srand(x);
-
-        sf::Font font;
-        font.loadFromFile("times.ttf");
-        sf::Text text("seed = " + to_string(x), font);
-        text.setCharacterSize(20);
-        text.setFillColor(sf::Color::Black);
-        text.setPosition(10,10);
-
-        sf::VertexArray dot(sf::Points, amount);
-        for(int i = 0; i < amount; ++i){
-            float a = static_cast<float>(100+rand()%800);
-            float b = static_cast<float>(100+rand()%400);
-            dot[i].position = sf::Vector2f(a, b);
-        }
-
-        //sf::VertexArray lines = andrewJarvis(dot);
-        //sf::VertexArray lines = fortune(dot);
-        sf::VertexArray lines = recursive(dot);
-
-        while (window.isOpen()){
-            sf::Event event;
-            while (window.pollEvent(event)){
-                if (event.type == sf::Event::Closed)
-                    window.close();
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "Lab3");
+    unsigned int x = time(0);
+    srand(x);
+    sf::VertexArray dot = generateDots();
+    //sf::VertexArray lines = andrewJarvis(dot);
+    //sf::VertexArray lines = fortune(dot);
+    sf::VertexArray lines = recursive(dot);
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Key::Space){
+                x = time(0);
+                srand(x);
+                dot = generateDots();
+                //lines = andrewJarvis(dot);
+                //lines = fortune(dot);
+                lines = recursive(dot);
             }
-            window.clear(sf::Color::White);
-            window.draw(text);
-            window.draw(lines);
-            for(int i = 0; i< amount; ++i)
-                drawDot(window, dot[i]);
-            window.display();
+
         }
+        window.clear(sf::Color::White);
+        drawInfo(window, x);
+        window.draw(lines);
+        for(int i = 0; i< amount; ++i)
+            drawDot(window, dot[i]);
+        window.display();
     }
     return 0;
 }
